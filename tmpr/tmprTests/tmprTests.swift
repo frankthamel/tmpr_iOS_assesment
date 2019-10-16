8177a95db96117bd8fb5bc457709c081495979c0
Frank Thamel <w.frankthamel@gmail.com>
2019-10-17 05:26:21 +0530

    initial commit

---
+++ tmpr/tmprTests/tmprTests.swift
@@ -0,0 +1,34 @@
+//
+//  tmprTests.swift
+//  tmprTests
+//
+//  Created by Frank Thamel on 17/10/2019.
+//  Copyright © 2019 Frank Thamel. All rights reserved.
+//
+
+import XCTest
+@testable import tmpr
+
+class tmprTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
